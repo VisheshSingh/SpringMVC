9818f5aa8da4431997ddcb317a749d2b7d1c2347
Vishesh <visheshsingh16@gmail.com>
2018-01-13 12:06:47 -0600

    Spring MVC-TextFields - Step1: Create Student class

---
+++ src/com/vishesh/springmvc/Student.java
@@ -0,0 +1,27 @@
+package com.vishesh.springmvc;
+
+public class Student {
+
+	private String firstName, lastName;
+
+	public Student() {
+
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
