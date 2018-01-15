a03cb95bc6d7b18932f207d4d5f39beaa4d8627c
Vishesh <visheshsingh16@gmail.com>
2018-01-14 19:34:27 -0600

    Add pre-processing code with initBinder to remove whte spaces

--- src/com/vishesh/springmvc/CustomerController.java
+++ src/com/vishesh/springmvc/CustomerController.java
@@ -2,15 +2,25 @@ package com.vishesh.springmvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+	// add initBinder.. to trim white spaces
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmer = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmer);
+	}
 
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
@@ -22,6 +32,7 @@ public class CustomerController {
 	@RequestMapping("/processForm")
 	public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
 
+		System.out.println("Last Name: |" + theCustomer.getLastName() + "|");
 		if (theBindingResult.hasErrors()) {
 			return "customer-form";
 		}
