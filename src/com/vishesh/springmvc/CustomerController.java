a91c2dd500fcf3529eff00d8cf7678fbb9eb81f7
Vishesh <visheshsingh16@gmail.com>
2018-01-14 18:45:36 -0600

    Validation - Step 4: Update confirmation page

--- src/com/vishesh/springmvc/CustomerController.java
+++ src/com/vishesh/springmvc/CustomerController.java
@@ -1,7 +1,11 @@
 package com.vishesh.springmvc;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,6 +17,15 @@ public class CustomerController {
 
 		theModel.addAttribute("customer", new Customer());
 		return "customer-form";
+	}
+
+	@RequestMapping("/processForm")
+	public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
+
+		if (theBindingResult.hasErrors()) {
+			return "customer-form";
+		}
+		return "customer-confirmation";
 	}
 
 }
