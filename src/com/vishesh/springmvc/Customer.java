6d8fa92deb01a57d635e80646af2f38b2e567a82
Vishesh <visheshsingh16@gmail.com>
2018-01-15 21:29:14 -0600

    Applying regex

--- src/com/vishesh/springmvc/Customer.java
+++ src/com/vishesh/springmvc/Customer.java
@@ -3,15 +3,12 @@ package com.vishesh.springmvc;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
 
 	private String firstName;
-
-	@Min(value = 0, message = "Enter a greater than 0")
-	@Max(value = 10, message = "Enter a less than or equal to 10")
-	private int freePasses;
 
 	@NotNull(message = "is required")
 	@Size(min = 1, message = "is required")
@@ -33,12 +30,27 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
+	@Min(value = 0, message = "Enter a greater than 0")
+	@Max(value = 10, message = "Enter a less than or equal to 10")
+	private int freePasses;
+
 	public int getFreePasses() {
 		return freePasses;
 	}
 
 	public void setFreePasses(int freePasses) {
 		this.freePasses = freePasses;
+	}
+
+	@Pattern(regexp = "^[0-9a-zA-z]{5}", message = "Only 5 characters")
+	private String postalCode;
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
 	}
 
 }
