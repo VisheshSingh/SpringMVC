8100b02b460417b485554952d6fa367f3014b340
Vishesh <visheshsingh16@gmail.com>
2018-01-17 09:33:32 -0600

    Create constraint validator

--- src/com/vishesh/springmvc/validation/CourseCode.java
+++ src/com/vishesh/springmvc/validation/CourseCode.java
@@ -1,12 +1,27 @@
 package com.vishesh.springmvc.validation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
 
 @Constraint(validatedBy = "CourseCodeConstraintValidator.class")
 @Target({ ElementType.METHOD, ElementType.FIELD })
-
+@Retention(RetentionPolicy.RUNTIME)
 public @interface CourseCode {
 
+	// define default course code
+	public String value() default "VTS";
+
+	// define default error message
+	public String message() default "must begin with VTS";
+
+	// define default groups
+	public Class<?>[] groups() default {};
+
+	// define default payloads
+	public Class<? extends Payload>[] payload() default {};
 }
